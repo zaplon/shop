456c84be8f64b37e925c6ec303406ec6c4b1ef8b
zaplon <janek.zapal@gmail.com>
2014-12-14 12:18:21 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='shop',
+    version='1.0',
+    packages=['shop', 'amsoil', 'amsoil.management', 'amsoil.management.commands', 'amsoil.migrations',
+              'amsoil.templatetags'],
+    url='',
+    license='',
+    author='jan',
+    author_email='janek.zapal@gmail.com',
+    description=''
+)
