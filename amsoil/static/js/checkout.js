47580f5ce645f629c484a69c29c95a800b961a2e
zaplon <janek.zapal@gmail.com>
2015-02-10 14:17:29 +0100

    Update checkout.js

--- amsoil/static/js/checkout.js
+++ amsoil/static/js/checkout.js
@@ -65,14 +65,14 @@ checkout = {
       if ($("input[name='shippingMethod']:checked").length > 0)
         var shipping = $("input[name='shippingMethod']:checked").val();
       else
-        if (parseInt($('input[name="shippingMethod"]').val() > 0))
+        if (parseInt($('input[name="shippingMethod"]').val()) > 0)
             var shipping = parseInt($('input[name="shippingMethod"]')).val();
         else
             var shipping = -1;
       if ($("input[name='paymentMethod']:checked").length > 0)
         var payment = $("input[name='paymentMethod']:checked").val();
       else
-        if (parseInt($('input[name="paymentMethod"]').val() > 0))
+        if (parseInt($('input[name="paymentMethod"]').val()) > 0)
             var payment = parseInt($('input[name="paymentMethod"]')).val();
         else
             var payment = -1;
