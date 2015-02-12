aa40c64351e5a86f7fe960f942a3b62573dd3f8b
zaplon <janek.zapal@gmail.com>
2015-02-12 12:30:31 +0100

    Update tags.py

--- amsoil/templatetags/tags.py
+++ amsoil/templatetags/tags.py
@@ -48,8 +48,8 @@ register.filter(placeholder)
 @register.inclusion_tag('priceFilter.html')
 def priceFilter():
     pvs = ProductVariation.objects.all()
-    minimum = pvs.aggregate(Min('price')).values()[0]
-    maximum = pvs.aggregate(Max('price')).values()[0]
+    minimum = int(pvs.aggregate(Min('price')).values()[0])
+    maximum = int(pvs.aggregate(Max('price')).values()[0])
     return {
     	'min': minimum,
     	'max': maximum
