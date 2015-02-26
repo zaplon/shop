c475ee662649a33fceaa70e59aaa0a6f7295cef3
zaplon <janek.zapal@gmail.com>
2015-02-26 10:13:59 +0100

    Update models.py

--- amsoil/models.py
+++ amsoil/models.py
@@ -21,6 +21,8 @@ class Template(models.Model):
         verbose_name_plural = 'Szablony'
     name = models.CharField(max_length=100)
     body = RichTextField(max_length=1000)
+    def __unicode__(self):
+        return self.name
 
 class Page(models.Model):
     class Meta:
