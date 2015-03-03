e5698fe8418713b5690af385819e06b61b4209d6
zaplon <janek.zapal@gmail.com>
2015-03-03 21:26:49 +0100

    dodatki

--- authentication/admin.py
+++ authentication/admin.py
@@ -25,7 +25,7 @@ class UserCreationForm(forms.ModelForm):
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
+            raise forms.ValidationError("Hasła nie są identyczne")
         return password2
 
     def save(self, commit=True):
