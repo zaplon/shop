c02844f583d04c4a8e09911e98240c28c8696159
zaplon <janek.zapal@gmail.com>
2015-03-03 19:45:48 +0100

    dodatki

--- amsoil/templatetags/tags.py
+++ amsoil/templatetags/tags.py
@@ -269,7 +269,7 @@ def breadcrumbs(context):
     i = 1
     for e in els:
         if len(e) > 0:
-            if e != 'product' and e != 'category':
+            if e not in ['product','category','atrybuty','kategorie']:
                 res.append({'url': '/'.join(els[0:i]), 'name': e})
         i = i + 1
     res[-1]['last'] = True
